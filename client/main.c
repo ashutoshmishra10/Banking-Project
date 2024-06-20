df1e156d6164cf06362ff8358588ca453266ded0
Ashutosh Mishra <78424828+ashutoshmishra10@users.noreply.github.com>
2024-06-20 10:27:42 +0530

    Create main.c

---
+++ client/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "socket_client.c"
+
+int main() {
+    // Implement client-side logic to interact with the server
+    printf("Bank client started...\n");
+
+    // Client logic here
+
+    return 0;
+}
