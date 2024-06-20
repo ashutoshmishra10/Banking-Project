cbea9081c8d158fa8fe88f41289c82e03349af24
Ashutosh Mishra <78424828+ashutoshmishra10@users.noreply.github.com>
2024-06-20 10:46:41 +0530

    Create bank.h

---
+++ common/bank.h
@@ -0,0 +1,37 @@
+// common/bank.h
+#ifndef BANK_H
+#define BANK_H
+
+#define MAX_CUSTOMERS 100
+#define MAX_NAME_LENGTH 50
+#define MAX_PASSBOOK_ENTRIES 100
+
+typedef struct {
+    int entry_number;
+    char description[100];
+    double amount;
+    double balance_after;
+} PassbookEntry;
+
+typedef struct {
+    int account_number;
+    char name[MAX_NAME_LENGTH];
+    double balance;
+    int is_joint;
+    PassbookEntry passbook[MAX_PASSBOOK_ENTRIES];
+    int passbook_entry_count;
+} Customer;
+
+typedef struct {
+    Customer customers[MAX_CUSTOMERS];
+    int customer_count;
+} Bank;
+
+void add_customer(Bank *bank, Customer customer);
+void remove_customer(Bank *bank, int account_number);
+Customer* get_customer(Bank *bank, int account_number);
+void deposit_money(Bank *bank, int account_number, double amount);
+void withdraw_money(Bank *bank, int account_number, double amount);
+void check_passbook(Customer *customer);
+
+#endif // BANK_H
