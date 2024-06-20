7586af0e5457603063b9f53d35ab743f47f37d3c
Ashutosh Mishra <78424828+ashutoshmishra10@users.noreply.github.com>
2024-06-20 10:41:59 +0530

    Update bank.h

--- bank.h
+++ bank.h
@@ -7,10 +7,19 @@
 #define MAX_PASSBOOK_ENTRIES 100
 
 typedef struct {
+    int entry_number;
+    char description[100];
+    double amount;
+    double balance_after;
+} PassbookEntry;
+
+typedef struct {
     int account_number;
     char name[MAX_NAME_LENGTH];
     double balance;
     int is_joint;
+    PassbookEntry passbook[MAX_PASSBOOK_ENTRIES];
+    int passbook_entry_count;
 } Customer;
 
 typedef struct {
@@ -18,18 +27,11 @@ typedef struct {
     int customer_count;
 } Bank;
 
-typedef struct {
-    int entry_number;
-    char description[100];
-    double amount;
-    double balance_after;
-} PassbookEntry;
-
 void add_customer(Bank *bank, Customer customer);
 void remove_customer(Bank *bank, int account_number);
 Customer* get_customer(Bank *bank, int account_number);
 void deposit_money(Bank *bank, int account_number, double amount);
 void withdraw_money(Bank *bank, int account_number, double amount);
-void check_passbook(PassbookEntry *passbook, int account_number);
+void check_passbook(Customer *customer);
 
 #endif // BANK_H
